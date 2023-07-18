f19dfe2e81d15cb7dd795eb3b95c6d3c2222c246
Leandro Bolivar <leandrob131@gmail.com>
2023-07-18 10:30:09 -0500

    Updated warps symbiote documentation

--- conf.py
+++ conf.py
@@ -858,7 +858,7 @@ GALLERIES_DEFAULT_THUMBNAIL = None
 
 IMAGE_FOLDERS = {'images': 'images'}
 # IMAGE_THUMBNAIL_SIZE = 400
-# IMAGE_THUMBNAIL_FORMAT = '{name}.thumbnail{ext}'
+IMAGE_THUMBNAIL_FORMAT = '{name}.thumbnail{ext}'
 
 # #############################################################################
 # HTML fragments and diverse things that are used by the templates
