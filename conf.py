5305790902f749971209e142f012680304d186b7
Leandro Bolivar <leandrob131@gmail.com>
2023-08-03 23:45:35 -0500

    fixed redirection

--- conf.py
+++ conf.py
@@ -668,7 +668,7 @@ ATOM_FILENAME_BASE = "feed"
 # relative URL.
 #
 # If you don't need any of these, just set to []
-REDIRECTIONS = [("/", "/blog")]
+REDIRECTIONS = [("/", "blog")]
 
 # Presets of commands to execute to deploy. Can be anything, for
 # example, you may use rsync:
